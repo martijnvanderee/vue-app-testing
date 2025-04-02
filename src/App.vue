95615946dd0af320929aa7d8b07aa83fdb894769
“broodbakker” <mrtjnvanderee@gmail.com>
2025-04-02 11:21:39 +0200

    added testing

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,13 @@
 <script setup lang="ts">
+import { useFetch } from '@vueuse/core';
+
+const { isFetching, error, data } = useFetch(
+  'http://vue-deno-39scww-31cecd-168-119-233-159.traefik.me/dinosaurs'
+);
+
+console.log('test');
+console.log('data', data, 'error:', error, isFetching);
+
 import Products from './components/Products.vue';
 import Header from './components/Header.vue';
 </script>
