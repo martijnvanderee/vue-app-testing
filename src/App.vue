62ed52768359dad8ebc8dabc891753d48f424893
“broodbakker” <mrtjnvanderee@gmail.com>
2025-04-03 13:14:38 +0200

    working app

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,14 @@
 import { useFetch } from '@vueuse/core';
 
 const takePayments = () => {
-  console.log('takePayments');
-  const { isFetching, error, data } = useFetch(
-    'http://vue-deno-39scww-31cecd-168-119-233-159.traefik.me/create-payment-intent'
+  useFetch(
+    'http://vue-deno-39scww-31cecd-168-119-233-159.traefik.me/create-payment-intent',
+    {
+      afterFetch(ctx) {
+        window.location.href = ctx.data;
+      },
+    }
   ).post({ items: 8 });
-
-  console.log('takePayments 1');
-  console.log('test');
-  console.log('data:', data, 'error:', error, isFetching);
-
-  // window.location.href = data.value;
 };
 
 import Products from './components/Products.vue';
