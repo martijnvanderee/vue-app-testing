fccb67bb4cf20bf917dd376dae47b0a3af9d4b08
“broodbakker” <mrtjnvanderee@gmail.com>
2025-04-03 13:35:04 +0200

    fix type issue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <script setup lang="ts">
 import { useFetch } from '@vueuse/core';
 
+const options = (ctx) => {
+  window.location.href = ctx.data;
+};
+
 const takePayments = () => {
   useFetch(
     'http://vue-deno-39scww-31cecd-168-119-233-159.traefik.me/create-payment-intent',
     {
       afterFetch(ctx) {
         window.location.href = ctx.data;
+
+        return ctx;
       },
     }
   ).post({ items: 8 });
