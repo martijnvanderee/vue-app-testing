8ca9fc35713ee29548533fad8cc372bb03df56ef
“broodbakker” <mrtjnvanderee@gmail.com>
2025-04-03 08:17:19 +0200

    send link to stripe server

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,25 @@
 <script setup lang="ts">
 import { useFetch } from '@vueuse/core';
-
-const { isFetching, error, data } = useFetch(
-  'http://vue-deno-39scww-31cecd-168-119-233-159.traefik.me'
-);
+import { loadStripe } from '@stripe/stripe-js';
 
 console.log('test');
 console.log('data', data, 'error:', error, isFetching);
+
+const takePayments = () => {
+  console.log('takePayments');
+  const { isFetching, error, data } = useFetch(
+    'http://vue-deno-39scww-31cecd-168-119-233-159.traefik.me/create-payment-intent'
+  ).post({ items: 8 });
+
+  console.log('takePayments 1');
+};
 
 import Products from './components/Products.vue';
 import Header from './components/Header.vue';
 </script>
 
 <template>
-  test 3:
+  <button @click="takePayments">payments</button>
   <Header />
   <Products />
 </template>
