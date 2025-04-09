35f95c67f8738c2ee9bec6a167cff15fda0b5e21
“broodbakker” <mrtjnvanderee@gmail.com>
2025-04-09 17:36:33 +0200

    test

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,21 @@
 import { RouterView } from 'vue-router';
 
 import Header from './components/Header.vue';
+
+import { useFetch } from '@vueuse/core';
+
+useFetch(
+  'vvue-backendpayload-zshvxw-d06432-168-119-233-159.traefik.me/api/products',
+  {
+    afterFetch(ctx) {
+      console.log('fetch');
+      console.log(ctx.data);
+      console.log(JSON.parse(ctx.data));
+
+      return ctx;
+    },
+  }
+).get();
 </script>
 
 <template>
